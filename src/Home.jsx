cd8283a56026ce6843bce803df823b5583cf03bd
krjofficial <krishrjain.official@gmail.com>
2024-05-22 22:34:49 +0530

    Output Lists

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
 
-const handleClick = () => {
-  console.log('clicked');
-}
+
 
 const Home = () => {
+
+  const [blogs, setBlog] = useState([
+    { title: 'My new Website', body: 'lorem ipsum', author: 'a', id: 1 },
+    { title: 'Welcome Party', body: 'lorem ipsum', author: 'b', id: 2 },
+    { title: 'Web Dev Topics', body: 'lorem ipsum', author: 'c', id: 3 }
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   )
 }
